e0c6c377992c3feeac71260f22155e0f4df23a29
RYUSEULGI <rhythm296@naver.com>
2023-12-04 00:47:56 +0900

    refactor: authOption 유틸 디렉토리로 분리

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,5 @@
-import prisma from '@/db'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import nextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/signin',
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-}
+import { authOptions } from '@/utils/authOptions'
+import nextAuth from 'next-auth'
 
 const handler = nextAuth(authOptions)
 
